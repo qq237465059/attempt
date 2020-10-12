25b8822a3225a008166592462d2e2e1ce3894f24
胡一 <yhu@ittx.com.cn>
2020-10-12 17:19:56 +0800

    init

--- src/main/java/com/hy233/attempt/Application.java
+++ src/main/java/com/hy233/attempt/Application.java
@@ -6,12 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigurationProperties
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, RedisAutoConfiguration.class})
 @ComponentScan(value = "com.hy233")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
